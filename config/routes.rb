39935bcaf54bed4ea8025c6795502d7610f17b23
Steve Polito <stevepolito@hey.com>
2021-07-03 12:46:12 -0400

    Routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    devise_scope :user do 
+      root "devise/registrations#edit", as: :authenticated_root
+    end
+  end
+
+  devise_scope :user do 
+    root "devise/sessions#new"
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
