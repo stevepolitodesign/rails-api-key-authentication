5d192f6b089bd6e77bfcac749e4a08342708d8cb
Steve Polito <stevepolito@hey.com>
2021-07-04 21:27:57 -0400

    Build private_api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_create :set_private_api_key
+
+  validates :private_api_key, uniqueness: true 
+
+  private
+
+      def set_private_api_key
+        self.private_api_key = SecureRandom.hex if self.private_api_key.nil?
+    end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: "unique_email@example.com", password: "password")
+  end 
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "set_private_api_key" do
+    assert_nil @user.private_api_key
+    @user.save
+    assert_not_nil @user.private_api_key
+  end
+
+  test "private_api_key should be unique" do
+    @user.save
+    @user_with_duplicate_private_api_key = User.new(email: "another_unique_email@example.com", password: "password", private_api_key: @user.private_api_key)
+    assert_not @user_with_duplicate_private_api_key.valid?
+  end
 end
