e40e2dcf5a88d59277f7a0a99dcb341801b70bd2
Steve Polito <stevepolito@hey.com>
2021-07-05 07:38:47 -0400

    Add route.
    Make it singular since we're always referencing the user.

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :user do
+    resource :private_api_keys, only: :update
+  end
 end
