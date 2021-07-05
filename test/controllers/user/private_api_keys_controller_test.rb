c1e19a0fc7bf16e875a1746c63133ec5831b5527
Steve Polito <stevepolito@hey.com>
2021-07-05 07:50:45 -0400

    user/private_api_keys#update

--- test/controllers/user/private_api_keys_controller_test.rb
+++ test/controllers/user/private_api_keys_controller_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class User::PrivateApiKeysControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "update" do
+    original_private_api_key = @user.private_api_key
+    sign_in @user
+    put user_private_api_keys_path
+    assert_not_equal original_private_api_key, @user.reload.private_api_key
+  end
 end
