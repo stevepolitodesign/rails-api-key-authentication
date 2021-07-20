16d342b41295a02c2422b8df4faf685348029817
Steve Polito <stevepolito@hey.com>
2021-07-20 06:42:20 -0400

    Replace private_api_key with private_api_key_ciphertext.

--- db/migrate/20210703170544_add_private_api_key_to_users.rb
+++ db/migrate/20210703170544_add_private_api_key_to_users.rb
@@ -1,6 +1,6 @@
 class AddPrivateApiKeyToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :private_api_key, :string, null: false
-    add_index :users, :private_api_key, unique: true
+    add_column :users, :private_api_key_ciphertext, :string, null: false
+    add_index :users, :private_api_key_ciphertext, unique: true
   end
 end
