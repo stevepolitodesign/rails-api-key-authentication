8a053753b82f81ee580e9e5638da240eff6edb31
Steve Polito <stevepolito@hey.com>
2021-07-22 06:01:45 -0400

    WIP: Build show action

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,4 +2,13 @@ class Api::V1::PostsController < Api::V1::BaseController
   def index
     @posts = @user.posts
   end
+
+  def show
+  end
+
+  private
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
