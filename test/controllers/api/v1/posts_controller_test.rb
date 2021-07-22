af0c0e86c4b0158245b7487c262fd6bf48a3e16f
Steve Polito <stevepolito@hey.com>
2021-07-22 05:49:15 -0400

    Test index action

--- test/controllers/api/v1/posts_controller_test.rb
+++ test/controllers/api/v1/posts_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class Api::V1::PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    # Using a fixture did not work. Intead, I needed to create a user
+    @user = User.create(email: "unique_email@example.com", password: "password")
+  end
+
+  class Authenticated < Api::V1::PostsControllerTest
+    test "should get posts" do
+      get api_v1_posts_path, headers: { "Authorization": "Token token=#{@user.private_api_key}" }
+      assert_response :ok
+    end
+  end
+
+  class Unauthorized < Api::V1::PostsControllerTest
+    test "should not get posts" do
+      get api_v1_posts_path
+      assert_response :unauthorized
+    end
+  end
+
 end
