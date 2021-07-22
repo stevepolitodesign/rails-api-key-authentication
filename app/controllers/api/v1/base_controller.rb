37fc7380f45e9ba6f9da9e0c9f842112b7d1b0fb
Steve Polito <stevepolito@hey.com>
2021-07-22 05:24:23 -0400

    WIP: Build authentication

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,12 +1,21 @@
 class Api::V1::BaseController < ApplicationController
-  before_action :authenticate_user_with_token
+  before_action :authenticate
 
   private
+
+    def authenticate
+      authenticate_user_with_token || handle_unauthorized
+    end
+
     # https://github.com/rails/rails/blob/83217025a171593547d1268651b446d3533e2019/actionpack/lib/action_controller/metal/http_authentication.rb#L352
     # https://www.pluralsight.com/blog/tutorials/token-based-authentication-rails
     def authenticate_user_with_token
       authenticate_with_http_token do |token, options|
-        User.find_by(private_api_key: token)
+        @user ||= User.find_by(private_api_key: token)
       end
+    end
+
+    def handle_unauthorized
+      render json: { message: "Bad credentials" }, status: 401
     end
 end
