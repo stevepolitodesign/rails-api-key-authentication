5ec94ac384113172d7b23c18ed7c55302bee0a08
Steve Polito <stevepolito@hey.com>
2021-07-25 17:12:24 -0400

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_143318) do
+ActiveRecord::Schema.define(version: 2021_07_25_211028) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 2021_07_18_143318) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "requestable_type", null: false
+    t.integer "requestable_id", null: false
+    t.integer "method", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["requestable_type", "requestable_id"], name: "index_requests_on_requestable"
+    t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +48,5 @@ ActiveRecord::Schema.define(version: 2021_07_18_143318) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "requests", "users"
 end
